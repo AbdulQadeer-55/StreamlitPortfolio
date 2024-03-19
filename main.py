153b8e8a395513245d44b35b88e6025e79be894c
Abdul Qadeer <qadeermemon100@gmail.com>
2024-03-19 20:33:18 +0500

    basic structure of streamlit app

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def main():
+    # Set the title and description of the app
+    st.title("My Streamlit App")
+    st.subheader("Welcome to my app!")
+
+    # Add your Streamlit components and functionality here
+
+if __name__ == "__main__":
+    main()
--- readme.md
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-    # Set page title
-    st.set_page_config(page_title="Streamlit Portfolio")
-
-    # Add your Streamlit app code here
-    st.title("Welcome to my Streamlit Portfolio!")
-    st.write("This is a sample Streamlit app.")
